2ccfd28e82311c8596b9666e823d6c7b72255a49
aidan_akamine <aidana@hawaii.edu>
2018-11-27 12:28:04 -1000

    added dystopian residule charts

--- overview_of_data_layout_variables.js
+++ overview_of_data_layout_variables.js
@@ -1,6 +1,5 @@
 
         var layout = [];
-
         
         layout['gdp'] = {
           hovermode: 'closest',
@@ -134,15 +133,16 @@
           title:'Happiness Score vs Government trustworthiness'
         }; 
 
-         layout['dystopia'] = {
+
+        layout['gdp_dystopia'] = {
           hovermode: 'closest',
           xaxis: {
-            title: 'dystopia score',
-            range: [ 0, 4 ]
+            title: 'GDP per capita score (higher means higher gdp per capita)',
+            range: [ 0, 2 ]
           },
           yaxis: {
-            title: 'Happiness score (higher is better)',
-            range: [2, 8]
+            title: 'dystopia score',
+            range: [0, 5]
           },
           legend: {
             y: 0.5,
@@ -153,5 +153,115 @@
               color: 'grey',
             }
           },
-          title:'Happiness Score vs dystopian residual'
+          title:'Dystopia Score vs GDP per capita'
+        };
+        
+         layout['family_dystopia'] = {
+          hovermode: 'closest',
+          xaxis: {
+            title: 'Family score',
+            range: [ 0, 2 ]
+          },
+          yaxis: {
+            title: 'dystopia score',
+            range: [0, 5]
+          },
+          legend: {
+            y: 0.5,
+            yref: 'paper',
+            font: {
+              family: 'Arial, sans-serif',
+              size: 20,
+              color: 'grey',
+            }
+          },
+          title:'Dystopia Score vs Family relations'
+        }; 
+
+         layout['health_dystopia'] = {
+          hovermode: 'closest',
+          xaxis: {
+            title: 'health (life expectency) score',
+            range: [ 0, 1.5 ]
+          },
+          yaxis: {
+            title: 'dystopia score',
+            range: [0, 5]
+          },
+          legend: {
+            y: 0.5,
+            yref: 'paper',
+            font: {
+              family: 'Arial, sans-serif',
+              size: 20,
+              color: 'grey',
+            }
+          },
+          title:'Dystopia Score vs Health(life expectency)'
+        }; 
+
+         layout['freedom_dystopia'] = {
+          hovermode: 'closest',
+          xaxis: {
+            title: 'Freedom score',
+            range: [ 0, 1 ]
+          },
+          yaxis: {
+            title: 'dystopia score',
+            range: [0, 5]
+          },
+          legend: {
+            y: 0.5,
+            yref: 'paper',
+            font: {
+              family: 'Arial, sans-serif',
+              size: 20,
+              color: 'grey',
+            }
+          },
+          title:'Dystopia Score vs Freedom'
+        }; 
+
+         layout['generosity_dystopia'] = {
+          hovermode: 'closest',
+          xaxis: {
+            title: 'Generosity score',
+            range: [ 0, 1 ]
+          },
+          yaxis: {
+            title: 'dystopia score',
+            range: [0, 5]
+          },
+          legend: {
+            y: 0.5,
+            yref: 'paper',
+            font: {
+              family: 'Arial, sans-serif',
+              size: 20,
+              color: 'grey',
+            }
+          },
+          title:'Dystopia Score vs Generosity'
+        }; 
+
+         layout['trust_dystopia'] = {
+          hovermode: 'closest',
+          xaxis: {
+            title: 'trust(government trustworthiness) score',
+            range: [ 0, 1 ]
+          },
+          yaxis: {
+            title: 'dystopia score',
+            range: [0, 5]
+          },
+          legend: {
+            y: 0.5,
+            yref: 'paper',
+            font: {
+              family: 'Arial, sans-serif',
+              size: 20,
+              color: 'grey',
+            }
+          },
+          title:'Dystopia Score vs Government trustworthiness'
         }; 
